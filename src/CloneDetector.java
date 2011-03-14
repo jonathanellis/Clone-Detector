5515cec78f4b0336d736af2d6c79193ad0d63aa1
Jonathan Ellis <jon@than.biz>
2011-03-14 17:45:49 +0000

    Basic single-line clone detection

--- src/CloneDetector.java
+++ src/CloneDetector.java
@@ -1,25 +1,67 @@
 import java.io.*;
 import java.security.*;
+import java.util.ArrayList;
 import java.math.*;
 
 
 public class CloneDetector {
+	public enum FingerprintMethod {MD5_HASH};
 
 	public CloneDetector(String filename) throws FileNotFoundException, IOException {
 		BufferedReader in = new BufferedReader(new FileReader(filename));
 		String line;
-
+		ArrayList<BigInteger> fingerprints = new ArrayList<BigInteger>();
+		
 		while ((line = in.readLine()) != null) {
-			try {
-				MessageDigest m = MessageDigest.getInstance("MD5");
-				m.update(line.getBytes(), 0, line.length());
-				System.out.println("hash: " + new BigInteger(1,m.digest()).toString(16));
-			} catch (NoSuchAlgorithmException e) {
-				e.printStackTrace();
+			String processedLine;
+			processedLine = stripWhitespace(line);
+			
+			BigInteger fingerprint;
+			if (processedLine.equals("")) {
+				fingerprint = BigInteger.ZERO;
+			} else {
+				fingerprint = computeFingerprint(processedLine, FingerprintMethod.MD5_HASH);
+			}
+			
+			//System.out.println(processedLine + " " + fingerprint);
+			
+			fingerprints.add(fingerprint);
+		}
+		
+		// Build comparison matrix between hashes:
+		for (int i=0; i<fingerprints.size(); i++) {
+			for (int j=0; j<i; j++) {
+				BigInteger fi = fingerprints.get(i);
+				BigInteger fj = fingerprints.get(j);
+				//System.out.println(fi);
+				if (fi.equals(fj) && !fi.equals(BigInteger.ZERO)) System.out.println("clone found! " + (i+1) + ":" + (j+1));
 			}
 			
 		}
 
+	}
+	
+	public BigInteger computeFingerprint(String line, FingerprintMethod method) {
+		BigInteger fingerprint = null;
+		if (method == FingerprintMethod.MD5_HASH) {
+			try {
+				MessageDigest m = MessageDigest.getInstance("MD5");
+				m.update(line.getBytes(), 0, line.length());
+				fingerprint = new BigInteger(1,m.digest());
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
+		}
+		return fingerprint;
+	}
+	
+	public String stripWhitespace(String line) {
+		String newLine;
+		newLine = line.replaceAll(" ", "");
+		newLine = newLine.replaceAll("\t", "");
+		newLine = newLine.replaceAll("\n", "");
+		newLine = newLine.replaceAll("\r", "");
+		return newLine;
 	}
 	
 	public static void main(String[] args) {
