16d6c14c9a3ab2cc5b6d6c77d7db46b3fda67cb3
Raunak Gupta <raunak.gupta@hotmail.com>
2011-03-31 19:19:46 +0100

    documented the code

--- src/uk/ac/ucl/cs/clonedetector/Clone.java
+++ src/uk/ac/ucl/cs/clonedetector/Clone.java
@@ -3,43 +3,46 @@
  */
 package uk.ac.ucl.cs.clonedetector;
 
-
 /**
- * Class definition
+ * <code>Clone</code> stores information about clones (starting position, and
+ * the length of the clone).
  */
 public class Clone {
 
 	/**
-	 * Instance variable def
+	 * The start of the first block that collides
 	 */
 	private int iStart;
-	
+
 	/**
-	 * Instance variable def
+	 * The start of the second block that collides
 	 */
 	private int jStart;
-	
+
 	/**
-	 * Instance variable def
+	 * The length is the length of the collision
 	 */
 	private int length;
-	
+
 	/**
 	 * Constructs a <code>Clone</code> using the passed parameters.
 	 * 
 	 * @param iStart
+	 *            start of the first block that collides
 	 * @param jStart
+	 *            start of the second block that collides
 	 * @param length
+	 *            length is the length of the collision
 	 */
 	public Clone(int iStart, int jStart, int length) {
 		this.iStart = iStart;
 		this.jStart = jStart;
 		this.length = length;
 	}
-	
+
 	@Override
 	public String toString() {
-		return String.format("%d-%d:%d-%d", this.iStart, this.iStart + this.length, 
-				this.jStart, this.jStart + length);
+		return String.format("%d-%d:%d-%d", this.iStart, this.iStart
+				+ this.length, this.jStart, this.jStart + length);
 	}
 }
