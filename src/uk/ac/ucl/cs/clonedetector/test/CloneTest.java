4955c58ce67538142db310ef3b73594fab453029
Tom Davies <czulu007@googlemail.com>
2011-03-31 17:29:31 +0100

    clone test class added

---
+++ src/uk/ac/ucl/cs/clonedetector/test/CloneTest.java
@@ -0,0 +1,37 @@
+package uk.ac.ucl.cs.clonedetector.test;
+
+import static org.junit.Assert.*;
+
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import uk.ac.ucl.cs.clonedetector.Clone;
+
+
+
+public class CloneTest {
+
+	public Clone c;
+	
+	@Before
+	public void setUp() throws Exception {
+		c = new Clone(1, 10, 3);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	
+	@Test
+	public void test_newClone() throws NoSuchAlgorithmException {
+		assertEquals(c.toString(), "1-4:10-13");
+	}
+	
+
+	
+}
