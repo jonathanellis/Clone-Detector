52088657b86f903a03f20a6f7d5c88764f07ecec
Raunak Gupta <raunak.gupta@hotmail.com>
2011-04-01 01:15:26 +0100

    saves file name as well as the line number

--- src/uk/ac/ucl/cs/clonedetector/Index.java
+++ src/uk/ac/ucl/cs/clonedetector/Index.java
@@ -5,22 +5,44 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Index {
-	private HashMap<BigInteger,ArrayList<Integer>> index = new HashMap<BigInteger,ArrayList<Integer>>();
-
-	public void updateIndex(BigInteger fingerprint, int lineNumber) {
-		if (fingerprint == BigInteger.ZERO) return;
-		
-		if (!index.containsKey(fingerprint)) {
-			index.put(fingerprint, new ArrayList<Integer>());
-		}
-		index.get(fingerprint).add(lineNumber);
-	}
 	
-	public ArrayList<Integer> linesWithFingerprint(BigInteger fingerprint) {
-		ArrayList<Integer> matches = index.get(fingerprint);
-		if (matches == null) return new ArrayList<Integer>();
+	private HashMap<BigInteger, ArrayList<CDoc>> index = new HashMap<BigInteger, ArrayList<CDoc>>();
+
+	public void updateIndex(BigInteger fingerprint, String fileName, int lineNumber) {
+		if (fingerprint == BigInteger.ZERO)
+			return;
+
+		if (!index.containsKey(fingerprint)) {
+			index.put(fingerprint, new ArrayList<CDoc>());
+		}
+
+		index.get(fingerprint).add(new CDoc(fileName, lineNumber));
+	}
+
+	public ArrayList<CDoc> linesWithFingerprint(BigInteger fingerprint) {
+		ArrayList<CDoc> matches = index.get(fingerprint);
+		
+		if (matches == null){
+			return new ArrayList<CDoc>();
+		}
 		return matches;
 	}
-	
-	
+
+	class CDoc {
+		private String fileName;
+		private int lineNumber;
+
+		public CDoc(String fileName, int lineNumber) {
+			this.fileName = fileName;
+			this.lineNumber = lineNumber;
+		}
+		
+		public String getFileName(){
+			return fileName;
+		}
+		
+		public int getLineNumber(){
+			return lineNumber;
+		}
+	}
 }
