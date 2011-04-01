be249c88d24090baa5a83868beef490e5eb3557e
Tom Davies <czulu007@googlemail.com>
2011-04-01 06:27:49 +0100

    clone testing

--- src/uk/ac/ucl/cs/clonedetector/test/CloneTest.java
+++ src/uk/ac/ucl/cs/clonedetector/test/CloneTest.java
@@ -1,6 +1,7 @@
 package uk.ac.ucl.cs.clonedetector.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.security.NoSuchAlgorithmException;
 
@@ -9,16 +10,27 @@ import org.junit.Before;
 import org.junit.Test;
 
 import uk.ac.ucl.cs.clonedetector.Clone;
-
-
+import uk.ac.ucl.cs.clonedetector.Reference;
 
 public class CloneTest {
 
 	public Clone c;
+	public Clone c2;
+	public Clone c3;
+	public Reference r;
+	public Reference r2;
+	public Reference r3;
+	public Reference r4;
 	
 	@Before
 	public void setUp() throws Exception {
-		//c = new Clone(1, 10, 3);
+		r = new Reference("filename", 14);
+		r2 = new Reference("filename", 15);
+		r3 = new Reference("filename", 16);
+		r4 = new Reference("filename", 17);
+		c = new Clone(r,r2,r3,r4);
+		c2 = new Clone(r3,r4,r,r2);
+		c3 = new Clone(r4,r2,r3,r);
 	}
 
 	@After
@@ -27,8 +39,24 @@ public class CloneTest {
 
 	
 	@Test
-	public void test_newClone() throws NoSuchAlgorithmException {
-		assertEquals(c.toString(), "1-4:10-13");
+	public void test_toString() {
+		String output = "(filename)14-15:(filename)16-17";
+		assertEquals(c.toString(), output);
+		
+		String output2 = "(filename)14-15:(filename)16-17";
+		assertEquals(c2.toString(), output2);
+	}
+	
+	@Test
+	public void test_getIStart() {
+		
+		assertEquals(c.getIStart(), r);
+	}
+	
+	@Test
+	public void test_compareTo() {
+		assertTrue(c.compareTo(c3) < 0);
+		assertTrue(c3.compareTo(c) > 0);
 	}
 	
 
