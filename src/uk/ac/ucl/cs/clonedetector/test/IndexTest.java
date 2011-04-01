76cfa19e5fdd33655194dcd84bec4b13bfa0b5f5
Tom Davies <czulu007@googlemail.com>
2011-04-01 05:52:19 +0100

    index testing added

--- src/uk/ac/ucl/cs/clonedetector/test/IndexTest.java
+++ src/uk/ac/ucl/cs/clonedetector/test/IndexTest.java
@@ -2,23 +2,31 @@ package uk.ac.ucl.cs.clonedetector.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import uk.ac.ucl.cs.clonedetector.Clone;
-
-
+import uk.ac.ucl.cs.clonedetector.Index;
+import uk.ac.ucl.cs.clonedetector.Reference;
 
 public class IndexTest {
+public Reference r;
+public Index i;
+public Index i2;
 
-	public Clone c;
-	
 	@Before
 	public void setUp() throws Exception {
-		//c = new Clone(1, 10, 3);
+		r = new Reference("filename", 14);
+		i = new Index();
+		i2 = new Index();
+		i.updateIndex(BigInteger.ONE, r);
+		i2.updateIndex(BigInteger.ZERO, r);
 	}
 
 	@After
@@ -27,8 +35,10 @@ public class IndexTest {
 
 	
 	@Test
-	public void test_newClone() throws NoSuchAlgorithmException {
-		assertEquals(c.toString(), "1-4:10-13");
+	public void test_lookup(){
+		assertEquals(i.lookup(BigInteger.ZERO), new ArrayList<Reference>());
+		assertEquals(i.lookup(BigInteger.ONE),new ArrayList<Reference>(Arrays.asList(r)));
+		assertEquals(i2.lookup(BigInteger.ZERO), new ArrayList<Reference>());
 	}
 	
 
